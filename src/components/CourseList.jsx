f9557006d50cfc9b0eeddae9c3b1c6939b83c1c0
VihaanShah26 <vihaanshah2026@u.northwestern.edu>
2024-10-03 18:54:57 -0500

    added styling

--- src/components/CourseList.jsx
+++ src/components/CourseList.jsx
@@ -1,8 +1,22 @@
+import './courseList.css';
+
 const CourseList = ({ courses }) => {
   return (
+    <div className="course-list">
+      {
         Object.entries(courses).map(([key, course]) => (
-            <p key={key}> {course.term} CS{course.number}: {course.title} </p>
+          <div className='card m-1 p-2'>
+            <div className='card-body'>
+              <h5 className='card-title'> {course.term} CS {course.number} </h5>
+              <p> {course.title} </p>
+            </div>
+          <div className='card-footer bg-transparent'>
+            <p className='card-text'> {course.meets} </p>
+          </div>
+          </div>
         ))
+      }
+      </div>
   );
 }
 
