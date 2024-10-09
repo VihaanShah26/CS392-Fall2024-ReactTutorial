0bc99835e3298866cd189c8fc64caf5fef54b4a8
VihaanShah26 <vihaanshah2026@u.northwestern.edu>
2024-10-09 15:04:22 -0500

    Added course selection

--- src/components/CourseList.jsx
+++ src/components/CourseList.jsx
@@ -1,13 +1,14 @@
 import './courseList.css';
 
-const CourseList = ({ courses , term}) => {
+const CourseList = ({ courses , term, selected, toggleSelected}) => {
+  console.log(selected);
   return (
     <div className="course-list">
       {
         Object.entries(courses)
           .filter(([key, course]) => course.term === term)
           .map(([key, course]) => (
-            <div key={key} className='card m-1 p-2'>
+            <div key={key} className={`card m1 p2 ${selected.includes(key) ? 'selected' : ''}`} onClick={() => toggleSelected(key)}>
               <div className='card-body'>
                 <h5 className='card-title'> {course.term} CS {course.number} </h5>
                 <p> {course.title} </p>
--- src/components/TermPage.jsx
+++ src/components/TermPage.jsx
@@ -6,32 +6,40 @@ import './TermPage.css';
 const terms = ["Fall", "Winter", "Spring"];
 
 const TermButton = ({ term, selection, setSelection }) => (
-  <div className='term-button'>
-    <input type="radio" id={term} className="btn-check" checked={term === selection} autoComplete="off"
-      onChange={() => setSelection(term)} />
-    <label className="btn btn-success mb-1 p-2" htmlFor={term}>
-      {term}
-    </label>
-  </div>
+    <div className='term-button'>
+        <input type="radio" id={term} className="btn-check" checked={term === selection} autoComplete="off"
+            onChange={() => setSelection(term)} />
+        <label className="btn btn-success mb-1 p-2" htmlFor={term}>
+            {term}
+        </label>
+    </div>
 );
 
 const TermSelector = ({ selection, setSelection }) => {
     return (
-      <div className="btn-group">
-        {terms.map((term) => (
-          <TermButton term={term} selection={selection} setSelection={setSelection} />
-        ))}
-      </div>
+        <div className="btn-group">
+            {terms.map((term) => (
+                <TermButton term={term} selection={selection} setSelection={setSelection} />
+            ))}
+        </div>
     );
-  };
+};
 
 const Page = ({ schedule }) => {
     const [selectedTerm, setSelectedTerm] = useState("Fall");
+    const [selected, setSelected] = useState([]);
+
+    const toggleSelected = (item) => setSelected(
+        selected.includes(item)
+            ? selected.filter(x => x !== item)
+            : [...selected, item]
+    );
+
     return (
         <div className="App">
             <Banner title={schedule.title} />
             <TermSelector selection={selectedTerm} setSelection={setSelectedTerm} />
-            <CourseList courses={schedule.courses} term={selectedTerm} />
+            <CourseList courses={schedule.courses} term={selectedTerm} selected={selected} toggleSelected={toggleSelected}/>
         </div>
     );
 };
