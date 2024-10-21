b5cfb9fc9007c61e41c3538314017b9d0b63ff23
VihaanShah26 <vihaanshah2026@u.northwestern.edu>
2024-10-20 22:55:11 -0500

    Set up database

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,13 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import useJsonQuery from './utilities/fetch';
 import Form from './components/EditForm';
 import { BrowserRouter, Routes, Route, useParams, useNavigate } from 'react-router-dom';
+import { useDbData } from "./utilities/firebase";
 
 const Main = () => {
-  const [data, isLoading, error] = useJsonQuery('https://courses.cs.northwestern.edu/394/guides/data/cs-courses.php');
-  if (error) return <h1>Error loading user data: {`${error}`}</h1>;
-  if (isLoading) return <h1>Loading user data...</h1>;
-  if (!data) return <h1>No user data found</h1>;
+  const [data, error] = useDbData('/');
+  if (error) return <h1>Error loading data: {error.toString()}</h1>;
+  if (data === undefined) return <h1>Loading data...</h1>;
+  if (!data) return <h1>No data found</h1>;
 
   const schedule = data;
 
---
+++ src/utilities/firebase.jsx
@@ -0,0 +1,55 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+import { useEffect, useState } from 'react';
+import { getDatabase, onValue, ref, update} from 'firebase/database';
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAtyQ3YuFYf7iFm6SsNuyCWJ7zasOec-48",
+  authDomain: "cs392-fall2024-react-tutorial.firebaseapp.com",
+  databaseURL: "https://cs392-fall2024-react-tutorial-default-rtdb.firebaseio.com",
+  projectId: "cs392-fall2024-react-tutorial",
+  storageBucket: "cs392-fall2024-react-tutorial.appspot.com",
+  messagingSenderId: "935231826085",
+  appId: "1:935231826085:web:0e5443e3a7f073afca454c",
+  measurementId: "G-PW2ZHPH2RW"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+const database = getDatabase(app);
+
+export const useDbData = (path) => {
+    const [data, setData] = useState();
+    const [error, setError] = useState(null);
+  
+    useEffect(() => (
+      onValue(ref(database, path), (snapshot) => {
+       setData( snapshot.val() );
+      }, (error) => {
+        setError(error);
+      })
+    ), [ path ]);
+  
+    return [ data, error ];
+  };
+  
+  const makeResult = (error) => {
+    const timestamp = Date.now();
+    const message = error?.message || `Updated: ${new Date(timestamp).toLocaleString()}`;
+    return { timestamp, error, message };
+  };
+  
+  export const useDbUpdate = (path) => {
+    const [result, setResult] = useState();
+    const updateData = useCallback((value) => {
+      update(ref(database, path), value)
+      .then(() => setResult(makeResult()))
+      .catch((error) => setResult(makeResult(error)))
+    }, [database, path]);
+  
+    return [updateData, result];
+  };
