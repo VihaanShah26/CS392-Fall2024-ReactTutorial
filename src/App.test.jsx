42c01112bec1db3311f81688ef6e7303c6ac23e4
VihaanShah26 <vihaanshah2026@u.northwestern.edu>
2024-10-25 15:51:42 -0500

    Added tets

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,19 +1,43 @@
-import {describe, expect, test} from 'vitest';
-import {fireEvent, render, screen} from '@testing-library/react';
+import {describe, it,} from 'vitest';
+import {render, screen} from '@testing-library/react';
 import App from './App';
 
-describe('counter tests', () => {
-    
-  test("Counter should be 0 at the start", () => {
-    render(<App />);
-    expect(screen.getByText('count is: 0')).toBeDefined();
-  });
+// describe('launching', () => {
+//   it('should show the current year', () => {
+//     render(<App />);
+//     screen.getByText(/2018/);
+//   });
+// });
 
-  test("Counter should increment by one when clicked", async () => {
-    render(<App />);
-    const counter = screen.getByRole('button');
-    fireEvent.click(counter);
-    expect(await screen.getByText('count is: 1')).toBeDefined();
-  });
+// describe('launching', () => {
+//   it('should show the current year', async () => {
+//     render(<App />);
+//     await screen.findByText(/2018/); // need to await to ensure that the promise response has arrived. othwerise test will always pass
+//   });
+// });
 
+import {useAuthState, useDbData} from './utilities/firebase';
+
+const mockSchedule = {
+  "title": "CS Courses for 1850-1851",
+  "courses": {
+  }
+};
+
+vi.mock('./utilities/firebase');
+
+beforeEach(() => {
+  useDbData.mockReturnValue([mockSchedule, null]);
+  useAuthState.mockReturnValue([null]);
+});
+
+afterEach(() => {
+  vi.resetAllMocks();
+});
+
+describe('launching', () => {
+  it('should show the current year', () => {
+    render(<App />);
+    screen.getByText(/1850/);
+  });
 });
