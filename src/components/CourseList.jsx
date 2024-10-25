1b39ecee8bf2b06dbda8f6e9822966c473ca8038
VihaanShah26 <vihaanshah2026@u.northwestern.edu>
2024-10-25 15:55:28 -0500

    Fixed bug

--- src/components/CourseList.jsx
+++ src/components/CourseList.jsx
@@ -1,21 +1,30 @@
 import './courseList.css';
 import CheckConflict from '../utilities/TimeConflict';
 import { Link } from 'react-router-dom';
-import {auth} from '../utilities/firebase';
+import { auth } from '../utilities/firebase';
 import { onAuthStateChanged } from "firebase/auth";
 import { useEffect, useState } from 'react';
+import { useProfile } from '../utilities/profile';
 
 const CourseList = ({ courses, term, selected, toggleSelected }) => {
   // console.log(courses);
   const [user, setUser] = useState(null);
 
-    useEffect(() => {
-        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
-            setUser(currentUser); 
-        });
+  useEffect(() => {
+    const unsubscribe = () => onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
 
-        return () => unsubscribe(); 
-    }, []);
+    return () => unsubscribe();
+  }, []);
+
+  const [profile, profileLoading, profileError] = useProfile();
+
+  if (profileError) return <h1>Error loading profile: {`${profileError}`}</h1>;
+  if (profileLoading) return <h1>Loading user profile</h1>;
+  if (!profile) return <h1>No profile data</h1>;
+
+  const isAdmin = profile.isAdmin;
 
   return (
     <div className="course-list">
@@ -36,7 +45,7 @@ const CourseList = ({ courses, term, selected, toggleSelected }) => {
               </div>
               <div className="card-footer bg-transparent">
                 <p className="card-text">{course.meets}
-                  {user && <Link to={`/edit/${key}`} onClick={(event) => event.stopPropagation()}>
+                  {isAdmin && <Link to={`/edit/${key}`} onClick={(event) => event.stopPropagation()}>
                     <i className="bi bi-pencil-square h4"></i>
                   </Link>}
                 </p>
